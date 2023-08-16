b5205ea5bc3d0e7649adf1fa293967c0ee8b5973
tonynealon1989@gmail.com <tony.nealon>
2023-08-16 17:52:11 +0000

    bump version

--- netbox_floorplan/__init__.py
+++ netbox_floorplan/__init__.py
@@ -5,7 +5,7 @@ class FloorplanConfig(PluginConfig):
     name = "netbox_floorplan"
     verbose_name = "Netbox Floorplan"
     description = ""
-    version = "0.1"
+    version = "0.1.1"
     base_url = "floorplan"
     min_version = "3.4.1"
 
