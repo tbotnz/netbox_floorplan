98584a2d667240fe29231ce931fc5fed1bde35b4
Justin Davis <justin.davis@us.ibm.com>
2023-09-19 09:42:06 -0400

    f

--- build/lib/netbox_floorplan/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Floorplan
-
-
-@admin.register(Floorplan)
-class FloorplanAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-    )
--- build/lib/netbox_floorplan/api/urls.py
+++
@@ -1,9 +0,0 @@
-from netbox.api.routers import NetBoxRouter
-from . import views
-
-app_name = 'netbox_floorplan'
-
-router = NetBoxRouter()
-router.register('floorplans', views.FloorplanViewSet)
-
-urlpatterns = router.urls
