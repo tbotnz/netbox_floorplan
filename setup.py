21e512a2c460367860193f8ebc6f17653d100b34
Nat Morris <nmorris@netboxlabs.com>
2023-10-31 16:45:12 +0000

    Update setup.py; fullname and repo url

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="netbox_floorplan",
+    name="netbox-floorplan-plugin",
     version="0.1.1",
     author="Tony Nealon",
     author_email="tony@worksystems.co.nz",
     description="Netbox Plugin to support graphical floorplans",
-    url="https://github.com/tbotnz/netbox_floorplan.git",
+    url="https://github.com/netboxlabs/netbox-floorplan-plugin.git",
     license="LGPLv3+",
     install_requires=[],
     packages=find_packages(),
